d0a364a5706df64b3d4e2965807b62aa717a3dfe
Jan Matusewicz <jan.matusewicz@bringmeister.de>
2018-04-24 11:42:57 +0200

    Do not use consumer group as oauth session identifier.

--- src/main/kotlin/de/bringmeister/spring/aws/kinesis/STSAssumeRoleSessionCredentialsProviderFactory.kt
+++ src/main/kotlin/de/bringmeister/spring/aws/kinesis/STSAssumeRoleSessionCredentialsProviderFactory.kt
@@ -3,22 +3,19 @@ package de.bringmeister.spring.aws.kinesis
 import com.amazonaws.auth.AWSCredentialsProvider
 import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider
 import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder
-import java.util.UUID
+import java.util.*
 
 class STSAssumeRoleSessionCredentialsProviderFactory(private val credentialsProvider: AWSCredentialsProvider,
                                                      private val settings: AwsKinesisSettings) : AWSCredentialsProviderFactory {
 
-     override fun credentials(roleToAssume: String) : AWSCredentialsProvider {
-         val roleSessionName = "${settings.consumerGroup}-${UUID.randomUUID()}"
-         return STSAssumeRoleSessionCredentialsProvider
-                                                     .Builder(roleToAssume, roleSessionName)
-                                                     .withStsClient(
-                                                         AWSSecurityTokenServiceClientBuilder
-                                                             .standard()
-                                                             .withRegion(settings.region)
-                                                             .withCredentials(credentialsProvider)
-                                                             .build()
-                                                     )
-                                                     .build()
-     }
+    override fun credentials(roleToAssume: String): AWSCredentialsProvider {
+        return STSAssumeRoleSessionCredentialsProvider
+                .Builder(roleToAssume, UUID.randomUUID().toString())
+                .withStsClient(AWSSecurityTokenServiceClientBuilder
+                        .standard()
+                        .withRegion(settings.region)
+                        .withCredentials(credentialsProvider)
+                        .build())
+                .build()
+    }
 }
