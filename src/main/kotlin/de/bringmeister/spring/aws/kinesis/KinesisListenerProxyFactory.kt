b476d945992b69f8deec1233751c523e389ac65f
Thomas Uhrig <thomas.uhrig@bringmeister.de>
2018-05-09 16:59:26 +0200

    Add support for proxies

--- src/main/kotlin/de/bringmeister/spring/aws/kinesis/KinesisListenerProxyFactory.kt
+++ src/main/kotlin/de/bringmeister/spring/aws/kinesis/KinesisListenerProxyFactory.kt
@@ -1,5 +1,8 @@
 package de.bringmeister.spring.aws.kinesis
 
+import org.springframework.aop.framework.Advised
+import org.springframework.aop.support.AopUtils
+
 /**
  * This class is a factory for [KinesisListenerProxy]. It takes any object and
  * looks for methods annotated with [KinesisListener]. For any of these methods
@@ -9,10 +12,22 @@ package de.bringmeister.spring.aws.kinesis
 class KinesisListenerProxyFactory {
 
     fun proxiesFor(bean: Any) : List<KinesisListenerProxy> {
-        return bean
+
+        // Since we are in a Sprint environment, it's very likely that
+        // we don't receive plain objects but AOP proxies. In order to
+        // work properly on those proxies, we need to "unwrap" them.
+        val isAopProxy = AopUtils.isAopProxy(bean)
+        var objectToProcess = if (isAopProxy && bean is Advised) {
+            bean.targetSource.target;
+        } else {
+            bean
+        }
+
+        return objectToProcess
                 .javaClass
                 .methods
                 .filter({ method -> method.isAnnotationPresent(KinesisListener::class.java) })
+                // the original bean! not the objectToProcess!
                 .map({ method -> KinesisListenerProxy(method, bean, method.getAnnotation(KinesisListener::class.java).stream) })
     }
 }
